dd29dd1193778bf1e1980363438c715199ee1737
ilkin.Aliev <stc_ilkin@mail.ru>
2022-05-27 20:50:38 +0300

    some fixes

--- HW_SEMINAR6/task_01.py
+++ HW_SEMINAR6/task_01.py
@@ -1,31 +1,17 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+n = int(input('задайте число '))
+n1 =n
+res = []
+def func(i):
+    global n1
+    global res
+    while (n1 % i == 0):
+        n1 //= i
+        res.append(i)
 
+list(map(func, [i for i in range(2, int(n**0.5)+1)]))
+if (n1 != 1):
+    res.append(n1)
 
-
-
-from math import sqrt
-n = int(input('Введите натуральное число для выведения списка простых множителей - '))
-simples=[2]
-for i in range(3, n+1, 2): #не проверяем числа делящиеся на 2
-    if (i > 10) and (i%10==5):
-        continue
-    for j in simples:
-        if j > int((sqrt(i)) + 1): #переберать надо только числа, не превосходящие корня из искомого
-            simples.append(i)
-            break
-        if (i % j == 0):
-            break
-    else:
-        simples.append(i)
-
-
-smpl_multip = []
-i = 0
-while n > 1:
-    if n % simples[i] == 0:
-        smpl_multip.append(simples[i])
-        n = n / simples[i]
-    else:
-        i += 1
-print(smpl_multip)
+print(res)
