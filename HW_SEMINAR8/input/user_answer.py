6ab6dae1644b94085784300d9f35affc8d792782
ilkin.Aliev <stc_ilkin@mail.ru>
2022-06-07 18:56:02 +0300

    HW_seminar8

---
+++ HW_SEMINAR8/input/user_answer.py
@@ -0,0 +1,29 @@
+
+
+
+def isNumber(string = 'Enter the number: '):
+    while True:
+        number = input(string)
+        if number.isdigit():
+            return int(number)
+
+
+
+user_answer = 0
+
+
+def read_user_answer(user_answer_text = 'Выберите операцию: ', minNumber = 0, maxNumber = 5):
+    global user_answer
+    while True:
+        user_answer = isNumber(user_answer_text)
+        if user_answer >= minNumber and user_answer <= maxNumber:
+            break
+    return user_answer
+
+
+def get_user_name():
+    return input('Введите имя:')
+
+
+def get_phone_number():
+    return input('номер телефона:')
